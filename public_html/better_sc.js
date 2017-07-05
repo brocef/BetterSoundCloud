d304390f1062a53548970b763495345de625dd5f
Brian Cefali <bcefali@192.168.56.1>
2017-07-05 13:14:45 -0400

    Proper sc item parsing, save album art

--- public_html/better_sc.js
+++ public_html/better_sc.js
@@ -1,3 +1,5 @@
+/* global chrome */
+
 (function(funcName, baseObj) {
     // The public function name defaults to window.docReady
     // but you can pass in your own object and own function name and those will be used
@@ -68,134 +70,95 @@
             }
             readyEventHandlersInstalled = true;
         }
-    }
+    };
 })("docReady", window);
 
-function filterSCItems(filterParams) {
-	var items = document.getElementsByClassName("soundList__item");
-	
-	for (var i=0; i<items.length; i++) {
-		var sc_item = items[i];
-		sc_item_map[sc_item] = {
-			visible: true,
-			parsed: false
-		};
-		parseSCResult(sc_item, filterParams, processSCObject);
-	}
+
+function parseSCItem(sc_item) {
+    var sel = function(selector) {
+        return sc_item.querySelector(selector);
+    };
+    
+    var artist_a = sel("div.soundTitle__secondary > a.soundTitle__username");
+    var reposter_a = sel("div.soundTitle__secondary > div.soundTitle__info > a.actorUser, div.activity > div.streamContext > div.soundContext > span.soundContext__line > a:first-child");
+    var track_a = sel("div.soundTitle__titleContainer > div > a.soundTitle__title");
+    var repost_time = sel("div.soundTitle__titleContainer > div.soundTitle__additionalContainer > div.soundTitle__uploadTime > time, div.activity > div.streamContext > div.soundContext > span.soundContext__line time");
+    var tags_a = sel("div.soundTitle__titleContainer > div.soundTitle__additionalContainer > div.soundTitle__tagContainer > a.soundTitle__tag");
+    var like_btn = sel("div.sound__footer > div.sound__soundActions button.sc-button-like");
+    var repost_btn = sel("div.sound__footer > div.sound__soundActions button.sc-button-repost");
+    var dl_a = sel("div.sound__footer > div.sound__soundActions a.soundActions__purchaseLink");
+    var plays_li = sel("div.sound__footer > div.sound__footerRight div.sound__soundStats li:nth-child(1)");
+    var comments_li = sel("div.sound__footer > div.sound__footerRight div.sound__soundStats li:nth-child(2)");
+    
+    
+    return {
+        artist_a: artist_a,
+        reposter_a: reposter_a,
+        track_a: track_a,
+        repost_time: repost_time,
+        tags_a: tags_a,
+        like_btn: like_btn,
+        repost_btn: repost_btn,
+        dl_a: dl_a,
+        plays_li: plays_li,
+        comments_li: comments_li
+    };
 }
 
-function processSCObject(sc_obj, filterParams) {
-	var min_duration_ms = filterParams.minimumTrackDuration * 1000;
-	var max_duration_ms = filterParams.maximumTrackDuration * 1000;
-
-	if (sc_obj.success) {
-		var t_dur = sc_obj.track.duration;
-		sc_obj.title_a.classList.toggle('errTitle', false);
-		if (t_dur < min_duration_ms || (max_duration_ms > 0 && t_dur > max_duration_ms)) {
-			hideSCResult(sc_obj, false, 'Duration out of range');
-		} else {
-			sc_obj.title_a.classList.add('seenTitle');
-		}
-	} else {
-		hideSCResult(sc_obj, true, "Error");
-	}
+function processSCItem(sc_item, cfg) {
+    var sc_obj = parseSCItem(sc_item);
+    console.log(sc_obj);
 }
 
-function showSCResult(sc_obj) {
-	
-}
-
-function hideSCResult(sc_obj, is_err, reason) {
-	if (!sc_item_map[sc_obj.sc_item].visible) return;
-	sc_item_map[sc_obj.sc_item].visible = false;
-	
-	var sc_activity = sc_obj.sc_item.childNodes[0];
-	sc_activity.classList.add('filteredTrack');
-	
-	var line = document.createElement("div");
-	sc_obj.sc_item.insertBefore(line, sc_activity);
-	
-	line.innerHTML = reason;
-	
-	line.classList.add('filteredLineDiv');
-}
-
-function parseSCResult(sc_item, filterParams, cb) {
-	var title_a = sc_item.querySelector(".soundTitle__title");
-	title_a.classList.add('errTitle');
-	
-	SC.resolve(title_a.href).then(function(track) {
-		cb({
-			success: true,
-			sc_item: sc_item,
-			track: track,
-			title_a: title_a
-		}, filterParams);
-	}, function(err) {
-		cb({
-			success: false,
-			sc_item: sc_item,
-			title_a: title_a,
-			err: err
-		}, filterParams);
-	});
+function initialParse(cfg) {
+    var sc_items = document.querySelectorAll("li.soundList__item");
+    for (var i=0; i<sc_items.length; i++) {
+        processSCItem(sc_items[i], cfg);
+    }
 }
 
 function getTargetList() {
-    return document.querySelector("#content .lazyLoadingList ul");
+    return document.querySelector("body > div#app > div#content div.userStream__list > ul.soundList, body > div#app > div#content div.stream__list > div.lazyLoadingList > ul.lazyLoadingList__list");
 }
-
-SC.initialize({
-  client_id: '5e776e8712475f822a3c91be4b547e0a'
-//	  client_secret: '334f743e9007a2a5e8b114dba590be42',
-//	  redirect_uri: 'http://localhost:5000'
-});
-
-var sc_objs = [];
-docReady(function() {	
-        init();
-});
 
 function init() {
     chrome.storage.sync.get({
 		minimumTrackDuration: 0,
 		maximumTrackDuration: -1
-	}, function(settings) {
+	}, function(cfg) {
 	
-	var target = getTargetList();
-	
-	// create an observer instance
-	var observer = new MutationObserver(function(mutations) {
-		
-		mutations.forEach(function(mutation) {
-			var new_nodes = mutation.addedNodes;
-			if (new_nodes.length != 1) {
-				console.log("Warning! Unexpected number of new children in single mutation");
-			}
-			var sc_item = new_nodes.item(0);
-			sc_item_map[sc_item] = {
-				visible: true,
-				parsed: false
-			};
-			parseSCResult(sc_item, settings, processSCObject);
-		});
-		
-		console.log(Object.keys(sc_item_map).length);
-		console.log(document.querySelectorAll(".soundList__item"));
-		
-		
-	});
-	
-	// configuration of the observer:
-	var config = { attributes: false, childList: true, characterData: false }
-	 
-	// pass in the target node, as well as the observer options
-	observer.observe(target, config);
-	 
-	// later, you can stop observing
-	// observer.disconnect();
-	
-	filterSCItems(settings);
+        var loopInject = function(){
+            if (getTargetList())
+            {   
+                var target = getTargetList();
+                // create an observer instance
+                var observer = new MutationObserver(function(mutations) {
+                        mutations.forEach(function(mutation) {
+                                var new_nodes = mutation.addedNodes;
+                                if (new_nodes.length !== 1) {
+                                        console.log("Warning! Unexpected number of new children in single mutation");
+                                }
+                                var sc_item = new_nodes.item(0);
+
+                                processSCItem(sc_item, cfg);
+                        });
+                });
+
+                // configuration of the observer:
+                var config = { attributes: false, childList: true, characterData: false };
+
+                // pass in the target node, as well as the observer options
+                observer.observe(target, config);
+
+                // later, you can stop observing
+                // observer.disconnect();
+
+                initialParse(cfg);
+                return 0;
+            }
+            setTimeout(loopInject, 5); // 5 ms
+        };
+        loopInject();
     });
 }
 
@@ -204,7 +167,7 @@ init_script.src = "chrome-extension://" + chrome.runtime.id + "/bsc_injected.js"
 // TODO: Better logic for inserting new script
 // Should probably insert into HEAD not HTML, but who knows what exists at the time
 var loopInject = function(){
-    if ( document.head )
+    if (document.head)
     {   
         document.head.appendChild(init_script);
         return 0;
@@ -212,3 +175,5 @@ var loopInject = function(){
     setTimeout(loopInject, 5); // 5 ms
 };
 loopInject();
+
+docReady(init);
--- public_html/bsc_injected.js
+++ public_html/bsc_injected.js
@@ -1,6 +1,14 @@
+/* global CanvasRenderingContext2D */
+
 CanvasRenderingContext2D.prototype.fillTextOrig = CanvasRenderingContext2D.prototype.fillText;
 CanvasRenderingContext2D.prototype.fillText = function(text, x, y, maxwidth) {
+    /*
     console.log("Drawing "+text+" at "+x+","+y);
     console.log(this.canvas);
+    */
+    if (!this.canvas.hasAttribute("duration") && text.match(/^(\d+:)?\d+:\d+$/)) {
+        this.canvas.setAttribute("duration", text);
+        this.canvas.classList.add("bscInitialized");
+    }
     this.fillTextOrig(text, x, y, maxwidth);
-}
+};
