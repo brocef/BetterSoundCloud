579c1ad1dd1c5b445a9c9673f491b315a11ec81f
Brian Cefali <bcefali@192.168.56.1>
2017-07-05 15:18:25 -0400

    Proper sc item parsing and loading hooks for delayed canvas duration

--- public_html/better_sc.js
+++ public_html/better_sc.js
@@ -1,5 +1,13 @@
 /* global chrome */
 
+/**
+ * Ignore this function--its a fancy way to do something when the document is loaded and ready.
+ * Equivalent to jQuery's $.ready()
+ * 
+ * @param {type} funcName
+ * @param {type} baseObj
+ * @returns {undefined}
+ */
 (function(funcName, baseObj) {
     // The public function name defaults to window.docReady
     // but you can pass in your own object and own function name and those will be used
@@ -73,35 +81,155 @@
     };
 })("docReady", window);
 
+function parseTimeString(hms_time) {
+    var dur_split = /(?:(\d+):)?(\d+):(\d+)/.exec(hms_time);
+    var hours = dur_split[1];
+    if (hours !== undefined) {
+        hours = parseInt(hours);
+    } else {
+        hours = 0;
+    }
+    var minutes = dur_split[2];
+    minutes = parseInt(minutes);
+    var seconds = dur_split[3];
+    seconds = parseInt(seconds);
+    
+    return seconds + (minutes * 60) + (hours * 60 * 60);
+}
+
+function parseFancyNumber(num) {
+    var spl = /^([\d,.]+)([MK])?$/.exec(num);
+    num = spl[1].split(",").join("");
+    num = parseFloat(num);
+    if (spl[2] === "K") {
+        num = num * 1000;
+    } else if (spl[2] === "M") {
+        num = num * 1000000;
+    }
+    return num;
+}
+
+function processSCItemWhenLoaded(sc_item, cfg) {
+    var getDurCanvas = function() {
+        return sc_item.querySelector("div.sound__waveform > div.waveform > div.waveform__layer > canvas.bscInitialized");
+    };
+    var waitForCanvas = function(){
+        if (getDurCanvas())
+        {
+            processSCItem(sc_item, cfg);
+            return 0;
+        }
+        setTimeout(waitForCanvas, 5); // 5 ms
+    };
+    return waitForCanvas();
+}
 
 function parseSCItem(sc_item) {
     var sel = function(selector) {
         return sc_item.querySelector(selector);
     };
     
+    var is_playlist = sel("div > div.playlist") !== null;
+    if (is_playlist) {
+        return {playlist: true};
+    }
+    
     var artist_a = sel("div.soundTitle__secondary > a.soundTitle__username");
     var reposter_a = sel("div.soundTitle__secondary > div.soundTitle__info > a.actorUser, div.activity > div.streamContext > div.soundContext > span.soundContext__line > a:first-child");
     var track_a = sel("div.soundTitle__titleContainer > div > a.soundTitle__title");
-    var repost_time = sel("div.soundTitle__titleContainer > div.soundTitle__additionalContainer > div.soundTitle__uploadTime > time, div.activity > div.streamContext > div.soundContext > span.soundContext__line time");
+    var post_time = sel("div.soundTitle__titleContainer > div.soundTitle__additionalContainer > div.soundTitle__uploadTime > time, div.activity > div.streamContext > div.soundContext > span.soundContext__line time");
     var tags_a = sel("div.soundTitle__titleContainer > div.soundTitle__additionalContainer > div.soundTitle__tagContainer > a.soundTitle__tag");
     var like_btn = sel("div.sound__footer > div.sound__soundActions button.sc-button-like");
     var repost_btn = sel("div.sound__footer > div.sound__soundActions button.sc-button-repost");
     var dl_a = sel("div.sound__footer > div.sound__soundActions a.soundActions__purchaseLink");
-    var plays_li = sel("div.sound__footer > div.sound__footerRight div.sound__soundStats li:nth-child(1)");
-    var comments_li = sel("div.sound__footer > div.sound__footerRight div.sound__soundStats li:nth-child(2)");
+    var plays_span = sel("div.sound__footer > div.sound__footerRight div.sound__soundStats li:nth-child(1) > span > span:nth-child(2)");
+    var comments_li = sel("div.sound__footer > div.sound__footerRight div.sound__soundStats li:nth-child(2) > a > span:nth-child(2)");
+    var duration_canvas = sel("div.sound__waveform > div.waveform > div.waveform__layer > canvas.bscInitialized");
     
+    var artist = {
+        name: artist_a.textContent.trim(),
+        link: artist_a.href
+    };
+    
+    var reposter = {
+        name: reposter_a.textContent.trim(),
+        link: reposter_a.href
+    };
+    
+    var track = {
+        name: track_a.textContent.trim(),
+        link: track_a.href
+    };
+    
+    if (post_time !== null) {
+        var timestamp = {
+            time: post_time.getAttribute('datetime')
+        };
+    } else {
+        var timestamp = null;
+    }
+    
+    var tags = [];
+    if (tags_a !== null) {
+        tags.push(tags_a.textContent.trim());
+    }
+    
+    var likes = parseFancyNumber(like_btn.textContent.trim());
+    
+    var reposts = parseFancyNumber(repost_btn.textContent.trim());
+    
+    if (dl_a !== null) {
+        var download = {
+            text: dl_a.textContent.trim(),
+            link: dl_a.href
+        };
+    } else {
+        var download = null;
+    }
+    
+    var plays = parseFancyNumber(plays_span.textContent.trim());
+    
+    if (comments_li !== null) {
+        var comments = parseFancyNumber(comments_li.textContent.trim());
+    } else {
+        var comments = null;
+    }
+    
+    var dur_raw = duration_canvas.getAttribute("duration").trim();
+    var dur = parseTimeString(dur_raw);
+    var duration = {
+        duration_raw: dur_raw,
+        duration: dur
+    };
     
     return {
-        artist_a: artist_a,
-        reposter_a: reposter_a,
-        track_a: track_a,
-        repost_time: repost_time,
-        tags_a: tags_a,
-        like_btn: like_btn,
-        repost_btn: repost_btn,
-        dl_a: dl_a,
-        plays_li: plays_li,
-        comments_li: comments_li
+        dom: {
+            artist_a: artist_a,
+            reposter_a: reposter_a,
+            track_a: track_a,
+            post_time: post_time,
+            tags_a: tags_a,
+            like_btn: like_btn,
+            repost_btn: repost_btn,
+            dl_a: dl_a,
+            plays_li: plays_span,
+            comments_li: comments_li,
+            duration_canvas: duration_canvas
+        },
+        values: {
+            artist: artist,
+            reposer: reposter,
+            track: track,
+            post_time: timestamp,
+            tags: tags,
+            likes: likes,
+            reposts: reposts,
+            download: download,
+            plays: plays,
+            comments: comments,
+            duration: duration
+        },
+        playlist: false
     };
 }
 
@@ -128,7 +256,7 @@ function init() {
 	}, function(cfg) {
 	
         var loopInject = function(){
-            if (getTargetList())
+            if (getTargetList() && document.querySelector("canvas.bscInitialized"))
             {   
                 var target = getTargetList();
                 // create an observer instance
@@ -139,8 +267,8 @@ function init() {
                                         console.log("Warning! Unexpected number of new children in single mutation");
                                 }
                                 var sc_item = new_nodes.item(0);
-
-                                processSCItem(sc_item, cfg);
+                                
+                                processSCItemWhenLoaded(sc_item, cfg);
                         });
                 });
 
