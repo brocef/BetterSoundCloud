c09df58198b98006076d60d49d7b23730cd3868f
Brian Cefali <brian.cefali@gmail.com>
2018-06-05 14:55:48 -0400

    Made style changes to options.html and its css file, also updated all the license headers to have 2018 instead of 2017

--- public_html/shared.js
+++ public_html/shared.js
@@ -1,5 +1,5 @@
 /* 
- * Copyright (C) 2017 Brian Cefali
+ * Copyright (C) 2018 Brian Cefali
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
